804e0fe86a174ec0aba06ac1f400e4ed4eef6684
Rahel Chowdhury <rahelahmed79@gmail.com>
2024-01-22 18:30:35 +0000

    some changes to composer DPM file and lock file, also service provider added and ACL file which will change..

---
+++ src/Acl/TraderPermission.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ *  @author: $rachow
+ *  @copyright: Coinhoppa
+ */
+
+namespace Coinhoppa\Acl;
+
+class TraderPermission
+{
+    const VIEW_ORDERS = 'view orders';
+    const EDIT_PROFILE = 'edit profile';
+}
---
+++ src/Providers/CoinhoppaServiceProvider.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ *  @author: $rachow
+ *  @copyright: Coinhoppa
+ */
+
+namespace Coinhoppa\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class CoinhoppaServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+}
