8f797f5cd2da2dc3e652f352a2e5a9a5ec8191c1
Rahel Chowdhury <rahelahmed79@gmail.com>
2024-03-20 16:05:08 +0000

    adding some changes and http connection events are now in action!

--- src/Providers/CoinhoppaEventServiceProvider.php
+++ src/Providers/CoinhoppaEventServiceProvider.php
@@ -9,9 +9,12 @@ namespace Coinhoppa\Providers;
 use Illuminate\Auth\Events\Registered;
 use Illuminate\Auth\Listeners\SendEmailVerificationNotification;
 use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
+use Illuminate\Http\Client\Events\RequestSending;
+use Illuminate\Http\Client\Events\ConnectionFailed;
 use Illuminate\Support\Facades\Event;
 use Coinhoppa\Observers\TraderObserver;
 use Coinhoppa\Models\Trader;
+
 
 class CoinhoppaEventServiceProvider extends ServiceProvider
 {
@@ -21,8 +24,23 @@ class CoinhoppaEventServiceProvider extends ServiceProvider
      * @var array
      */
     protected $listen = [
-
+        'Illuminate\Http\Client\Events\RequestSending' => [
+            'Coinhoppa\Listeners\LogHttpRequestSending'
+        ],
+        'Illuminate\Http\Client\Events\ConnectionFailed' => [
+            'Coinhoppa\Listeners\LogHttpConnectionFailed'
+        ]
     ];
+
+    /**
+     * Register any services
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
 
     /**
      * Register any events for your application.
@@ -32,8 +50,7 @@ class CoinhoppaEventServiceProvider extends ServiceProvider
     public function boot()
     {
         parent::boot();
-
-        // listening to event
+        
         Trader::observe(TraderObserver::class);
     }
 }
