2c9e1ef3d1cd4b87c89a23d0626d259c303aff10
Rahel Chowdhury <rahelahmed79@gmail.com>
2024-03-20 13:11:00 +0000

    refactoring the helper along with new LUA talking to redis hook and new build query func

--- src/Helpers/Common.php
+++ src/Helpers/Common.php
@@ -11,8 +11,12 @@ use Illuminate\Support\Str;
 
 class Common
 {
-    // get user agenst string
-    public static function fetchUserAgent()
+    /**
+	 * Grab the user agent string.
+	 * 
+	 * @param none
+	 */
+    public static function fetchUserAgent(): string
     {
         $agents = [
             "Mozilla/5.0 (Windows; U; Windows NT 5.1; en-GB; rv:1.8.1.6) Gecko/20070725 Firefox/2.0.0.6",
@@ -20,8 +24,12 @@ class Common
         return $agents[0];
     }
 
-    // grab the database neutral format for dates.
-    public static function fetchDateTimeParts($dateTimeStr)
+    /**
+	 * Grab the database native date formats.
+	 * 
+	 * @var $dateTimeStr
+	 */
+    public static function fetchDateTimeParts($dateTimeStr): string
     {
 		if ($dateTimeStr == '' || $dateTimeStr == null) {
 			return '';
@@ -64,8 +72,11 @@ class Common
         return $dateTimeStr;
     }
 
-    // get phone based on E.164 standards for storing.
-    // @var $countryCode ISO 3166-1 (alpha-2)
+	/**
+	 * Grab the phone number based on the E.164 standards for storing.
+	 * 
+	 * @var $countryCode = ISO 3166-1 (alpha-2)
+	 */
     public static function getE164PhoneNumber($phone, $countryCode = 'GB')
     {
         try {
@@ -75,4 +86,56 @@ class Common
 		    return $phone;
 		}
     }
+
+	/**
+	 * Not used, but LUA will be used for Redis comms.
+	 * 
+	 * @param none
+	 */
+	public static function releaseLock()
+	{
+		$args = func_get_args();
+		return <<<RELEASE_LUA
+			if redis.call("get",KEYS[1]) == $args[1] then
+				return redis.call("del",KEYS[1])
+			else
+				return 0
+			end
+RELEASE_LUA;
+	}
+
+	/**
+	 * Build the URL or URI with provided params.
+	 * 
+	 * @param $url
+	 * @param $params
+	 */
+	public static function buildQuery($url, array $params): string
+	{
+        if (! preg_match("/^[a-zA-Z]+:\/\//", $url)) {
+            if (preg_match("/\?/", $url)) {
+                list($uri, $query) = explode('?', $url);
+                if (! empty($query)) {
+                    parse_str($query, $queryParams);
+                    $params = array_merge($queryParams, $params);
+                    $url = rtrim($uri, '/');
+                }
+            }
+            
+            return sprintf('%s/?%s', $url, http_build_query($params));
+
+        } else {
+            
+            $queryParams = [];
+		    $urlParsed = parse_url($url);
+		    parse_str(array_key_exists('query', $urlParsed) ? $urlParsed['query'] : '', $queryParams);
+		    $queryParams = array_merge($queryParams, $params);
+		    return sprintf('%s://%s%s/?%s',
+			    $urlParsed['scheme'],
+			    $urlParsed['host'],
+			    $urlParsed['path'],
+			    http_build_query($queryParams)
+		    );
+	    }
+	}
 }
