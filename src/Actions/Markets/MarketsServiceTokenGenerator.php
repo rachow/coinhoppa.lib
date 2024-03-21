a3fd2e1fe53ebc1a4f810dac972ed47870a5d276
Rahel Chowdhury <rahelahmed79@gmail.com>
2024-03-21 15:08:01 +0000

    adding the action that will take care of any token regenerations

---
+++ src/Actions/Markets/MarketsServiceTokenGenerator.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ *  @author: $rachow
+ *  @copyright: Coinhoppa
+ */
+
+namespace Coinhoppa\Actions\Markets;
+
+use Illuminate\Support\Facades\Log;
+use Illuminate\Http\Request;
+
+class MarketsServiceTokenGenerator
+{
+    public function __construct(protected $service){}
+
+    public function handle(string $token)
+    {
+        $this->log('Attempting to generate token for ' . $this->service);
+        return $token;
+    }
+
+    private function log($message)
+    {
+        Log::info($message);
+    }
+}
